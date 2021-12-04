4b8b8d05eaa39d9a847c6a15255881ecbc247de2
DESKTOP-T103B5K\siru <afa890419@gmail.com>
2021-12-04 16:04:45 +0800

    Complete button_2 which in Button folder

---
+++ office/Assets/Scenes/Button/src/ButtonClick_main_1.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BC_1 : MonoBehaviour
+{
+    public int buttonAmount = 10;
+    public int sevenSegmentAmount = 3;
+    public MyButton[] numberButton = new MyButton[10];
+    public MyButton enterButton;
+    public MyButton clearButton;
+    public SevenSegment[] seven = new SevenSegment[3];
+
+    public int[] answerNumber = new int[10];
+    private int[] clickedButtonNumber = new int[10];
+    private int clickedButtonNumber_index = 0;
+    private bool answerCorrect = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (answerCorrect == false && clickedButtonNumber_index != sevenSegmentAmount)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0)) { ClickButton(numberButton[0], 0); } // Input number 0
+            if (Input.GetKeyDown(KeyCode.Alpha1)) { ClickButton(numberButton[1], 1); } // Input number 1
+            if (Input.GetKeyDown(KeyCode.Alpha2)) { ClickButton(numberButton[2], 2); } // Input number 2
+            if (Input.GetKeyDown(KeyCode.Alpha3)) { ClickButton(numberButton[3], 3); } // Input number 3
+            if (Input.GetKeyDown(KeyCode.Alpha4)) { ClickButton(numberButton[4], 4); } // Input number 4
+            if (Input.GetKeyDown(KeyCode.Alpha5)) { ClickButton(numberButton[5], 5); } // Input number 5
+            if (Input.GetKeyDown(KeyCode.Alpha6)) { ClickButton(numberButton[6], 6); } // Input number 6
+            if (Input.GetKeyDown(KeyCode.Alpha7)) { ClickButton(numberButton[7], 7); } // Input number 7
+            if (Input.GetKeyDown(KeyCode.Alpha8)) { ClickButton(numberButton[8], 8); } // Input number 8
+            if (Input.GetKeyDown(KeyCode.Alpha9)) { ClickButton(numberButton[9], 9); } // Input number 9
+        }
+        else if (answerCorrect == false && clickedButtonNumber_index == sevenSegmentAmount)
+        {
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                SegmentRefresh();
+                clickedButtonNumber_index = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) { checkAnswer(); }
+    }
+    void ClickButton(MyButton NbButton, int n)
+    {
+        clickedButtonNumber[clickedButtonNumber_index] = n;
+        NbButton.ChangeButtonColor();
+        seven[clickedButtonNumber_index].SetSegment(n);
+        clickedButtonNumber_index += 1;
+    }
+
+    /*
+    void ClickButton(GameObject NbButton, int n)
+    {
+        clickedButtonNumber[clickedButtonNumber_index] = n;
+        //NbButton.ChangeButtonColor();
+
+        NbButton.GetComponent<MeshRenderer>().material.color = Color.gray; //按鍵按下後，改變一下顏色
+        //Thread.Sleep(1);
+        NbButton.GetComponent<MeshRenderer>().material.color = Color.white;
+        seven[clickedButtonNumber_index].SetSegment(n);
+        clickedButtonNumber_index += 1;
+    }*/
+
+    public void SegmentRefresh()
+    {
+        for (int i = 0; i < seven.Length; i++)
+        {
+            seven[i].SetSegment(-1);
+        }
+    }
+
+    private void checkAnswer()
+    {
+        answerCorrect = true;
+        //按數字鍵1時，變更material顏色
+        //紀錄按過的數字
+        for (int i = 0; i < sevenSegmentAmount; i++)
+        {
+            if (answerNumber[i] != clickedButtonNumber[i])
+            {
+                answerCorrect = false;
+                break;
+            }
+        }
+
+        if (answerCorrect == true)
+        {
+
+            for (int i = 0; i < buttonAmount; i++)
+            {
+                numberButton[i].ChangeButtonColor_yellow();
+            }
+            enterButton.ChangeButtonColor_yellow();
+            clearButton.ChangeButtonColor_yellow();
+        }
+        else
+        {
+            SegmentRefresh();
+        }
+        clickedButtonNumber_index = 0;
+    }
+}
---
+++ office/Assets/Scenes/Button/src/ButtonClick_main_2.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick_main_2 : MonoBehaviour
+{
+    public int buttonAmount = 2;
+    public int sevenSegmentAmount = 2;
+    public MyButton[] accumulateButtons = new MyButton[2];
+    public MyButton enterButton;
+    public MyButton clearButton;
+    public SevenSegment[] seven = new SevenSegment[2];
+    public int[] answerNumber = new int[2];
+    private int[] clickedButtonNumber = new int[2];
+    private bool answerCorrect = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < sevenSegmentAmount; i++)
+        {
+            clickedButtonNumber[i] = 0;
+            seven[i].SetSegment(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (answerCorrect == false)
+        {
+            if (Input.GetKeyDown(KeyCode.L)) { ClickButton(accumulateButtons[0], 0, ++clickedButtonNumber[0]); } // Input left, left + 1
+            if (Input.GetKeyDown(KeyCode.R)) { ClickButton(accumulateButtons[1], 1, ++clickedButtonNumber[1]); } // Input right, right + 1
+            if (Input.GetKeyDown(KeyCode.Delete)){ SegmentRefresh(); }
+            clickedButtonNumber[0] = clickedButtonNumber[0] % 10;
+            clickedButtonNumber[1] = clickedButtonNumber[1] % 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) { checkAnswer(); }
+        
+    }
+    void ClickButton(MyButton NbButton,int index, int n)
+    {
+        NbButton.ChangeButtonColor();
+        seven[index].SetSegment(n % 10);
+    }
+
+    private void checkAnswer()
+    {
+        answerCorrect = true;
+        //按數字鍵1時，變更material顏色
+        //紀錄按過的數字
+        for (int i = 0; i < sevenSegmentAmount; i++)
+        {
+            if (answerNumber[i] != clickedButtonNumber[i])
+            {
+                answerCorrect = false;
+                break;
+            }
+        }
+
+        if (answerCorrect == true)
+        {
+            for (int i = 0; i < buttonAmount; i++)
+            {
+                accumulateButtons[i].ChangeButtonColor_yellow();
+            }
+            enterButton.ChangeButtonColor_yellow();
+            clearButton.ChangeButtonColor_yellow();
+        }
+        else
+        {
+            SegmentRefresh();
+        }
+    }
+    public void SegmentRefresh()
+    {
+        for (int i = 0; i < sevenSegmentAmount; i++)
+        {
+            clickedButtonNumber[i] = 0;
+            seven[i].SetSegment(0);
+        }
+    }
+}
